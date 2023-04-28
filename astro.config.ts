733f8196c96cf9fbaf876057858f46f08ddc3715
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-04-28 15:59:51 -0400

    chore: watch environment

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
+const debugBuild = (
+  opts?: Partial<{ importMeta: boolean; processEnv: boolean }>
+) => {
+  const filteredOpts = opts || { importMeta: false, processEnv: false };
+  const { importMeta, processEnv } = filteredOpts;
+
+  if (importMeta) console.log("importMeta: ", import.meta.env);
+  if (processEnv) console.log("processEnv: ", process.env);
+};
+
+debugBuild({ importMeta: true });
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
