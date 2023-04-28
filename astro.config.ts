23cfe179bc0c293730b6709a631150eb3ee7ff1e
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-04-28 14:17:13 -0400

    fix: try again to deal with env

--- astro.config.ts
+++ astro.config.ts
@@ -3,16 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-const site = process.env.VERCEL_URL
-  ? process.env.VERCEL_URL
-  : "http://localhost:3000";
-
-// console.log(`site: ${site}`);
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [solidJs(), tailwind()],
   adapter: vercel(),
-  site,
+  site: "https://the-landing.vercel.app",
 });
--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -2,7 +2,17 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import SuperJSON from "superjson";
 import type { AppRouter } from "./server";
 
+const getUrl = () => {
+  const base = import.meta.env.DEV
+    ? "http://localhost:3000"
+    : process.env.VERCEL_URL
+    ? process.env.VERCEL_URL
+    : import.meta.env.SITE;
+
+  return base + "/api/trpc";
+};
+
 export const trpcClientSide = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: import.meta.env.SITE + "/api/trpc" })],
+  links: [httpBatchLink({ url: getUrl() })],
   transformer: SuperJSON,
 });
